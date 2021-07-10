517c9ef806068e16a6b72b522b7d3b212230bc24
Bradley Playdon <61969883+bradleyplaydon@users.noreply.github.com>
2021-07-10 15:12:20 +0100

    Connect to DB with var to show skins

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask
+from flask import (Flask, render_template, 
+    request, redirect, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
@@ -13,14 +14,15 @@ app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 
 mongo = PyMongo(app)
+skins = mongo.db.skins.find()
 
 @app.route('/')
-def test():
-    return 'test'
+def index():
+    return render_template("pages/index.html", skins=skins)
 
 if __name__ == '__main__':
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
         debug=True
-    )
+    )  
