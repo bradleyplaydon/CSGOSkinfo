3efcfc8e3488eedbe185bf0dfbc8b38d13266118
Bradley Playdon <61969883+bradleyplaydon@users.noreply.github.com>
2021-07-10 12:18:02 +0100

    Connected Flask app to MongoDB

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 import os
 from flask import Flask
+from flask_pymongo import flask_PyMongo
+from bson.objectid import objectid
+
+if os.path.exists("env.py"):
+    import env
+
 
 app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+
+mongo = PyMongo(app)
 
 @app.route('/')
 def test():
