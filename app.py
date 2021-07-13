11d13fa8b9e289f7ff0ea02da1d79eda9c3a5232
Bradley Playdon <61969883+bradleyplaydon@users.noreply.github.com>
2021-07-13 15:23:38 +0100

    Added signup function to add user to db

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import os
 from flask import (Flask, render_template, 
-    request, redirect, url_for)
+    request, redirect, url_for, flash, session)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -23,7 +23,40 @@ def index():
     return render_template("pages/index.html", page_title="Latest Skins", skins=skins)
 
 
-@app.route('/add/skin')
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        if request.form.get("password") != request.form.get("confirmpassword"):
+            flash("Passwords doesn't match") 
+            return redirect(url_for("register"))
+
+        register = {
+            "first_name": request.form.get("firstName").capitalize(),
+            "last_name": request.form.get("lastName").capitalize(),
+            "username": request.form.get("username").lower(),
+            "email_address": request.form.get("email"),
+            "password": generate_password_hash(request.form.get("password")),
+            "is_admin": "false",
+            "skins_liked": list(),
+            "skins_disliked": list()
+        }    
+
+        mongo.db.users.insert_one(register)
+
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Succesful")
+        return redirect(url_for("account", username=session["user"]))
+
+    return render_template("pages/signup.html", login=False, page_title="Sign Up")
+
+@app.route('/add/skin', methods=["GET", "POST"])
 def add_skin():
     skinColl.insert_one(request.form.to_dict())
     return render_template("components/add-skin.html", page_title="Latest Skins")
