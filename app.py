983300cd28ed00e8c69cda5f15c56480b84cf33e
Bradley Playdon <61969883+bradleyplaydon@users.noreply.github.com>
2021-07-13 14:56:46 +0100

    Added function to insert skin to db

--- app.py
+++ app.py
@@ -14,11 +14,19 @@ app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 
 mongo = PyMongo(app)
-skins = mongo.db.skins.find()
+
+skinColl = mongo.db.skins
+skins = skinColl.find()
 
 @app.route('/')
 def index():
-    return render_template("pages/index.html", skins=skins)
+    return render_template("pages/index.html", page_title="Latest Skins", skins=skins)
+
+
+@app.route('/add/skin')
+def add_skin():
+    skinColl.insert_one(request.form.to_dict())
+    return render_template("components/add-skin.html", page_title="Latest Skins")
 
 if __name__ == '__main__':
     app.run(
