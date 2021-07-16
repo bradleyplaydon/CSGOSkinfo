11d56b8ce94ca5732496286ce56270cbce73e141
Bradley Playdon <61969883+bradleyplaydon@users.noreply.github.com>
2021-07-16 23:34:24 +0100

    Added function to check if skin exists

--- static/js/addskin.js
+++ static/js/addskin.js
@@ -42,76 +42,112 @@ $(document).ready(function () {
         conditionEls.not(this).prop('required', !$(this).val().length);
     });
 
-    const myDatePicker = MCDatepicker.create({ 
+    const myDatePicker = MCDatepicker.create({
         el: '#release-date',
-        dateFormat: 'YYYY-MM-DD', 
+        dateFormat: 'YYYY-MM-DD',
     })
 
-    $("#weapon-skin-form").on("change", function(){
+    $("#weapon-skin-form").on("change", function () {
         console.log($("#weapon-skin-form").find("[name=name]").val())
     })
 
     $("#weapon-skin-form").submit(function (e) {
         e.preventDefault();
+
         if (checkedCount < 3) {
             $("#invalid-error").text("Please tick 3 conditions in order to add the skin and add images");
-            $('#invalid-error').fadeIn('slow', function(){
-                $('#invalid-error').delay(2000).fadeOut(); 
-             });
+            $('#invalid-error').fadeIn('slow', function () {
+                $('#invalid-error').delay(2000).fadeOut();
+            });
         } else {
             $("#invalid-error").addClass("d-none");
-            insertSkin($(this))
+            myDatePicker.getFullDate();
+            checkSkinInsert($(this))
         }
         var data = $(this).serializeArray();
     })
 
 });
 
-
-function insertSkin(thisObj){
-    
-    var rarity = thisObj.find("[name=rarity]").val();
-        var rariryPrecedence = rarity == "Contraband" ? 6 
-                             : rarity == "Covert" ? 5 
-                             : rarity == "Classified" ? 4 
-                             : rarity == "Restricted" ? 3 
-                             : rarity == "Mil-Spec Grade" ? 2
-                             : rarity == "Industrial Grade" ? 1 
-                             : "Consumer Grade"
-                             
-        fetch("/insert/weapon", {
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                method: "POST",
-                body: JSON.stringify({
-                    name: thisObj.find("[name=name]").val(),
-                    skin_description: thisObj.find("[name=skin_description]").val(),
-                    type: thisObj.find("[name=type]").val(),
-                    weapon_type: thisObj.find("[name=weapon_type]").val(),
-                    weapon_name: thisObj.find("[name=weapon_name]").val(),
-                    rarity: rarity,
-                    rarity_precedence: rariryPrecedence,
-                    souvenir_available: thisObj.find("[name=stat_or_souv]").val() == "Souvenir" ? true : false,
-                    stattrak_available: thisObj.find("[name=stat_or_souv]").val() == "StatTrak" ? true : false,
-                    statrak_conditions: {
-                        factory_new: thisObj.find("[name=factory_new]").checked == true ? true : false,
-                        min_wear: thisObj.find("[name=min_wear]").checked == true ? true : false,
-                        field_tested: thisObj.find("[name=field_tested]").checked == true ? true : false,
-                        well_worn: thisObj.find("[name=well_worn]").checked == true ? true : false,
-                        battle_scarred: thisObj.find("[name=battle_scarred]").checked == true ? true : false
-                    },
-                    conditions: {
-                        factory_new: thisObj.find("[name=factory_new]").checked == true ? true : false,
-                        min_wear: thisObj.find("[name=min_wear]").checked == true ? true : false,
-                        field_tested: thisObj.find("[name=field_tested]").checked == true ? true : false,
-                        well_worn: thisObj.find("[name=well_worn]").checked == true ? true : false,
-                        battle_scarred: thisObj.find("[name=battle_scarred]").checked == true ? true : false
-                    }
-                })
-            }).then(res => {
-                thisObj.find("strong").text(thisObj.find("strong").val() + 1)
-                thisObj.attr("already-liked", true)
+function checkSkinInsert(thisObj) {
+    var skinName = thisObj.find("[name=name]").val()
+    fetch("/get/skin", {
+            headers: {
+                "Content-Type": "application/json"
+            },
+            method: "POST",
+            body: JSON.stringify({
+                name: skinName
             })
-            .catch(err => console.log(err))
+        }).then(res => res.text())
+        .then(data => {
+            if (data == "False") {
+                insertSkin(thisObj)
+            } else {
+                alert("Sorry this couldn't be added as a search was made and this skin has already been added.")
+            }
+        })
+
+        .catch(err => console.log(err))
+}
+
+
+
+function insertSkin(thisObj) {
+
+    var rarity = thisObj.find("[name=rarity]").val();
+    var rariryPrecedence = rarity == "Contraband" ? 6 :
+        rarity == "Covert" ? 5 :
+        rarity == "Classified" ? 4 :
+        rarity == "Restricted" ? 3 :
+        rarity == "Mil-Spec Grade" ? 2 :
+        rarity == "Industrial Grade" ? 1 :
+        "Consumer Grade"
+
+    fetch("/insert/weapon", {
+            headers: {
+                "Content-Type": "application/json"
+            },
+            method: "POST",
+            body: JSON.stringify({
+                name: thisObj.find("[name=name]").val(),
+                skin_description: thisObj.find("[name=skin_description]").val(),
+                type: thisObj.find("[name=type]").val(),
+                weapon_type: thisObj.find("[name=weapon_type]").val(),
+                weapon_name: thisObj.find("[name=weapon_name]").val(),
+                rarity: rarity,
+                rarity_precedence: rariryPrecedence,
+                souvenir_available: thisObj.find("[name=stat_or_souv]").val() == "Souvenir" ? true : false,
+                stattrak_available: thisObj.find("[name=stat_or_souv]").val() == "StatTrak" ? true : false,
+                statrak_conditions: {
+                    factory_new: thisObj.find("[name=factory_new]").checked == true ? true : false,
+                    min_wear: thisObj.find("[name=min_wear]").checked == true ? true : false,
+                    field_tested: thisObj.find("[name=field_tested]").checked == true ? true : false,
+                    well_worn: thisObj.find("[name=well_worn]").checked == true ? true : false,
+                    battle_scarred: thisObj.find("[name=battle_scarred]").checked == true ? true : false
+                },
+                conditions: {
+                    factory_new: thisObj.find("[name=factory_new]").checked == true ? true : false,
+                    min_wear: thisObj.find("[name=min_wear]").checked == true ? true : false,
+                    field_tested: thisObj.find("[name=field_tested]").checked == true ? true : false,
+                    well_worn: thisObj.find("[name=well_worn]").checked == true ? true : false,
+                    battle_scarred: thisObj.find("[name=battle_scarred]").checked == true ? true : false
+                },
+                image_urls: {
+                    factory_new: thisObj.find('input[name=fnimage]').val() == "" ? null : "https://community.cloudflare.steamstatic.com/economy/image/" + thisObj.find('input[name=fnimage]').val(),
+                    min_wear: thisObj.find('input[name=mwimage]').val() == "" ? null : "https://community.cloudflare.steamstatic.com/economy/image/" + thisObj.find('input[name=mwimage]').val(),
+                    field_tested: thisObj.find('input[name=ftimage]').val() == "" ? null : "https://community.cloudflare.steamstatic.com/economy/image/" + thisObj.find('input[name=ftimage]').val(),
+                    well_worn: thisObj.find('input[name=wwimage]').val() == "" ? null : "https://community.cloudflare.steamstatic.com/economy/image/" + thisObj.find('input[name=wwimage]').val(),
+                    battle_scarred: thisObj.find('input[name=bsimage]').val() == "" ? null : "https://community.cloudflare.steamstatic.com/economy/image/" + thisObj.find('input[name=bsimage]').val()
+                },
+                up_votes: 0,
+                down_votes: 0
+            })
+        }).then(res => {
+            var formSubmitted = thisObj;
+            formSubmitted.prev(`<h4 class='text-center bg-success p-3'>The ${thisObj.find("[name=name]").val()} has been succesfully added }}'</h4><button class='btn btn-orange'>Add Another</button>`)
+            formSubmitted.remove();
+
+        })
+        .catch(err => console.log(err))
 }
