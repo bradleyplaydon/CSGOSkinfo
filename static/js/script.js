d795b5f5aa6a34fcb051b512996b4aba505f4e99
Bradley Playdon <61969883+bradleyplaydon@users.noreply.github.com>
2021-07-19 21:16:38 +0100

    Added front-end click listeners

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,41 @@
+$(".like-btn").on("click", function (e) {
+    e.preventDefault();
+    if (!$(this).attr("already-liked")) {
+        like($(this));
+    } else {
+        unlike($(this));
+    }
+});
+
+
+$(".dislike-btn").on("click", function (e) {
+    e.preventDefault();
+    if ($(this).attr("already-liked")) {
+        unlike($(this));
+        dislike($(this));
+    } else {
+        dislike($(this));
+    }
+});
+
+
+function like(thisObj){
+
+}
+
+function unlike(thisObj){
+
+}
+
+function dislike(thisObj){
+
+}
+
+function undislike(thisObj){
+
+}
+
+
 $('.most-popular').slick({
     autoplay: true,
     autoplaySpeed: 1800,
@@ -7,3 +45,6 @@ $('.most-popular').slick({
     slidesToShow: 1,
     slidesToScroll: 1
 });
+
+
+$("#copyright").text(new Date().getFullYear())
