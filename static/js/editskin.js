7eb68199523f264ea20165b20b7884f0637f8a8a
Bradley Playdon <61969883+bradleyplaydon@users.noreply.github.com>
2021-07-19 12:31:22 +0100

    Added dynamic select options js

---
+++ static/js/editskin.js
@@ -0,0 +1,19 @@
+$(document).ready(function ($) {
+    let weaponTypeSelectEl = $("#skin-weapon-type");
+    let weaponNameSelectEl = $("#skin-weapon-name");
+    let checkRadioOptions = $("#neither, #stattrak, #souvenir")
+    var weapons = JSON.parse($("#weapons").attr("data-weapons"));
+    setWeaponSelectOptions(weaponTypeSelectEl.val().toLowerCase());
+    $(weaponTypeSelectEl).change(function () {
+        $(weaponNameSelectEl).empty();
+        setWeaponSelectOptions(weaponTypeSelectEl.val().toLowerCase());
+    });
+
+    function setWeaponSelectOptions(selectVal) {
+        weapons[selectVal].map((w, index) => {
+            o = new Option(w, w);
+            $(o).html(w);
+            $(weaponNameSelectEl).append(o);
+        });
+    }
+});
