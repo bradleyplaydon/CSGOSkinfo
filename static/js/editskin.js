bc5b6c027f18b4be4cfb6ead14e30543aee48182
Bradley Playdon <61969883+bradleyplaydon@users.noreply.github.com>
2021-07-19 15:33:49 +0100

    Removed fun function on load

--- static/js/editskin.js
+++ static/js/editskin.js
@@ -1,8 +1,8 @@
-$(document).ready(function ($) {
+$(document).ready(function () {
     let weaponTypeSelectEl = $("#skin-weapon-type");
     let weaponNameSelectEl = $("#skin-weapon-name");
     var weapons = JSON.parse($("#weapons").attr("data-weapons"));
-    setWeaponSelectOptions(weaponTypeSelectEl.val().toLowerCase());
+
     $(weaponTypeSelectEl).change(function () {
         $(weaponNameSelectEl).empty();
         setWeaponSelectOptions(weaponTypeSelectEl.val().toLowerCase());
@@ -40,7 +40,6 @@ $(document).ready(function ($) {
     preForm = $("#edit-skin-form").serialize();
     
     $("#edit-skin-form").on("change", function () {
-        console.log($(this).serialize(), preForm)
         if ($(this).serialize() === preForm) {
             $(this).find("#edit-skin").attr("disabled", "disabled")
         } else {
