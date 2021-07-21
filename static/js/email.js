23bd585557afd54de13b22f4845edf379a24cd96
Bradley Playdon <61969883+bradleyplaydon@users.noreply.github.com>
2021-07-21 03:33:16 +0100

    Added data var object

--- static/js/email.js
+++ static/js/email.js
@@ -1,3 +1,18 @@
 const serivceID = "service_irmiak4";
 const templateID = "service_irmiak4";
 const userID = "user_IZexurD0xkQkoR2UK3flk";
+
+$(document).ready(function() {
+  var data = {
+    service_id: serivceID,
+    template_id: templateID,
+    user_id: userID,
+    template_params: {
+      'from_name': "",
+      'from_email': "",
+      'contact_reason': "",
+      'message': ""
+    }
+  };
+  
+});
