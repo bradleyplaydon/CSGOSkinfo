b24ce5c0f9b6f090238e9fea4e9afd988c44cdec
Bradley Playdon <61969883+bradleyplaydon@users.noreply.github.com>
2021-07-21 14:24:53 +0100

    Added back to top button js

--- static/js/script.js
+++ static/js/script.js
@@ -152,4 +152,19 @@ $('.slider').slick({
     ]
 });
 
+var btn = $('#btt');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.removeClass('d-none');
+  } else {
+    btn.addClass('d-none');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '500');
+});
+
 $("#copyright").text(new Date().getFullYear())
