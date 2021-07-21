b861968d8ce8e1032267a0e721099863f80b000e
Bradley Playdon <61969883+bradleyplaydon@users.noreply.github.com>
2021-07-21 04:27:53 +0100

    Added slick slider for liked skins

--- static/js/script.js
+++ static/js/script.js
@@ -115,4 +115,41 @@ $('.most-popular').slick({
     slidesToScroll: 1
 });
 
+$('.slider').slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                infinite: true,
+                dots: false,
+                arrows: false
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: false,
+                arrows: false
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: false,
+                arrows: false
+            }
+        }
+    ]
+});
+
 $("#copyright").text(new Date().getFullYear())
